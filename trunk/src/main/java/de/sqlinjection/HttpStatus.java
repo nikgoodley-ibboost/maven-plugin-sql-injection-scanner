56757672594e53298aab924fa2ddfbe3cbada1b3
Nataliya.Shevchenko@gmail.com <Nataliya.Shevchenko@gmail.com@772861dd-1d62-cf6b-fec9-7aff7cb9eea6>
2012-05-10 13:13:12 +0000

    check if site available

---
+++ trunk/src/main/java/de/sqlinjection/HttpStatus.java
@@ -0,0 +1,13 @@
+package de.sqlinjection;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nshevchenko
+ * Date: 10.05.12
+ * Time: 15:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class HttpStatus {
+    
+    public static final int SUCCESS = 200;
+}
