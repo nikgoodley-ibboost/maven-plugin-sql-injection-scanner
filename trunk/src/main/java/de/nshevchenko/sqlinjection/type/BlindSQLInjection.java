744a0e8a34903320cfc50c560c31d0adae6dbd01
Nataliya.Shevchenko@gmail.com <Nataliya.Shevchenko@gmail.com@772861dd-1d62-cf6b-fec9-7aff7cb9eea6>
2012-05-18 15:13:48 +0000

    error based and blind

---
+++ trunk/src/main/java/de/nshevchenko/sqlinjection/type/BlindSQLInjection.java
@@ -0,0 +1,11 @@
+package de.nshevchenko.sqlinjection.type;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: nshevchenko
+ * Date: 18.05.12
+ * Time: 17:10
+ * To change this template use File | Settings | File Templates.
+ */
+public class BlindSQLInjection {
+}
