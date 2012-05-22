a72d79a629f3652891ff73350c336f160a6a3b97
Nataliya.Shevchenko <Nataliya.Shevchenko@gmail.com>
2012-05-22 16:23:37 +0000

    cleaned modifying params with the correct results

--- src/main/java/de/nshevchenko/sqlinjection/check/DbPayload.java
+++ src/main/java/de/nshevchenko/sqlinjection/check/DbPayload.java
@@ -2,14 +2,16 @@ package de.nshevchenko.sqlinjection.check;
 
 
 import de.nshevchenko.sqlinjection.type.SQLInjectionTypes;
+import org.apache.log4j.Logger;
 
 public class DbPayload {
-
+    private Logger log = Logger.getLogger(DbPayload.class);
     private  SQLInjectionTypes injectionTypes = new SQLInjectionTypes();
     private int indexOfActiveSQlInjectionPayload = 0;
     
     public String nextPayload(String oldValue){
         String payload = injectionTypes.getPayloadForIndex(indexOfActiveSQlInjectionPayload, oldValue);
+        log.debug("SQLInjection TYPE: "+injectionTypes.getPayloadForIndex(indexOfActiveSQlInjectionPayload, oldValue));
         indexOfActiveSQlInjectionPayload++;
         return payload;
     }
--- src/main/java/de/nshevchenko/sqlinjection/check/anchor/CheckAnchor.java
+++ src/main/java/de/nshevchenko/sqlinjection/check/anchor/CheckAnchor.java
@@ -9,33 +9,54 @@ import org.apache.log4j.Logger;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
 public class CheckAnchor {
-    static Logger log = Logger.getLogger(CheckAnchor.class);
+    private Logger log = Logger.getLogger(CheckAnchor.class);
 
+    private static final String PARAM_VALUE_SEPARATOR = "=";
+    
     public void checkAnchors(HtmlPage originalPage, PageFetcher pageFetcher) {
         List<HtmlAnchor> anchors = originalPage.getAnchors();
         Set<String> checkedParams = new HashSet<String>();
 
         for (HtmlAnchor anchor : anchors) {
-            List<String> paramsInAnchor = new ArrayList<String>();
+            Map<String, String> paramsInAnchor = new HashMap<String, String>();
 
             String hrefAttr = anchor.getHrefAttribute();
             int indexOfQuestionMark = hrefAttr.indexOf("?");
             String paramString = hrefAttr.substring(indexOfQuestionMark + 1);
-            fetchParamNames( paramsInAnchor, paramString);
-            if (!checkedParams.containsAll(paramsInAnchor)) {
-                for (String paramNameInAnchor : paramsInAnchor) {
+
+            Matcher m = Pattern.compile("&").matcher(paramString);
+
+            int start = 0;
+            String paramName = null;
+            while (m.find(start)) {
+
+                MatchResult matchResult = m.toMatchResult();
+                String paramWithValue = paramString.substring(start, matchResult.start());
+                paramName = paramWithValue.substring(0, paramWithValue.indexOf(PARAM_VALUE_SEPARATOR));
+                start = matchResult.end();
+                paramsInAnchor.put(paramName, paramWithValue.substring(paramWithValue.indexOf(PARAM_VALUE_SEPARATOR)+PARAM_VALUE_SEPARATOR.length()));
+            }
+            //put in the last param
+            if(start!=paramString.length()){
+                String lastParam = paramString.substring(start);
+                paramName = lastParam.substring(0, lastParam.indexOf(PARAM_VALUE_SEPARATOR)) ;
+                paramsInAnchor.put(paramName, lastParam.substring(lastParam.indexOf(PARAM_VALUE_SEPARATOR)+PARAM_VALUE_SEPARATOR.length()));
+            }
+
+
+
+            if (!checkedParams.containsAll(paramsInAnchor.keySet())) {
+                for (String paramNameInAnchor : paramsInAnchor.keySet()) {
+
                     try {
-                        checkAnchor(pageFetcher, originalPage.getFullyQualifiedUrl(anchor.getHrefAttribute()), paramNameInAnchor);
+                        checkAnchor(pageFetcher, originalPage.getFullyQualifiedUrl(anchor.getHrefAttribute()), paramsInAnchor, paramNameInAnchor);
                     } catch (MalformedURLException e) {
                         //TODO show to the user
                         log.error(e);
@@ -43,61 +64,38 @@ public class CheckAnchor {
                 }
 
             }
-            checkedParams.addAll(paramsInAnchor);
+            checkedParams.addAll(paramsInAnchor.keySet());
 
         }
 
     }
 
-    public void fetchParamNames( List<String> paramsInAnchor, String paramString) {
-        Matcher m = Pattern.compile("&").matcher(paramString);
 
-        int start = 0;
-        String paramName = null;
-        while (m.find(start)) {
-
-            MatchResult matchResult = m.toMatchResult();
-            String paramWithValue = paramString.substring(start, matchResult.start());
-            paramName = paramWithValue.substring(0, paramWithValue.indexOf("="));
-            start = matchResult.end();
-            paramsInAnchor.add(paramName);
-        }
-        //put in the last param
-        if(start!=paramString.length()){
-            String lastParam = paramString.substring(start);
-            paramName = lastParam.substring(0, lastParam.indexOf("=")) ;
-            paramsInAnchor.add(paramName);
-        }
-       
-
-    }
-    
-    public static void main(String [] args){
-        CheckAnchor checkAnchor = new CheckAnchor();
-        List<String> list = new ArrayList<String>();
-        checkAnchor.fetchParamNames(list, "action=artikel&cat=8&id=17&artlang=de");
-        System.out.println(list.size());
-    }
-
-    private void checkAnchor(PageFetcher pageFetcher, URL url, String paramNameToCheck) {
+    private void checkAnchor(PageFetcher pageFetcher, URL url, Map<String, String> paramsInAnchor, String paramNameToCheck) {
         HtmlPage originalPage = pageFetcher.getHtmlPageForUrl(url.toString());
         String urlString = url.toString();
-        int indexOfParamValue = urlString.indexOf(paramNameToCheck + "=");
+        int indexOfParamValue = urlString.indexOf(paramNameToCheck + PARAM_VALUE_SEPARATOR);
         DbPayload payload = new DbPayload();
+
         while (payload.hasMorePayloads()) {
 
             if (indexOfParamValue > 0) {
 
-                int indexOfEndOfValue = urlString.substring(indexOfParamValue).indexOf("&");
-                int lengthUntilParamStringEndFromStart = indexOfParamValue + indexOfEndOfValue;
-                String urlToModify = new String(urlString.substring(0, lengthUntilParamStringEndFromStart));
+                StringBuffer newUrl = new StringBuffer(urlString.substring(0, urlString.indexOf("?")+1));
 
-                String stringWithParamName = urlToModify.substring(0, urlToModify.lastIndexOf("=")+1);
-                String valueToModify = urlToModify.substring(urlToModify.lastIndexOf("=")+1);
+                for(Map.Entry<String, String> paramWithValue: paramsInAnchor.entrySet()){
+                    newUrl.append(paramWithValue.getKey());
+                    newUrl.append(PARAM_VALUE_SEPARATOR);
+                    if(paramWithValue.getKey().equals(paramNameToCheck))
+                    {
+                         newUrl.append(payload.nextPayload(paramWithValue.getValue()));
+                    }
+                    else{
+                        newUrl.append(paramWithValue.getValue());
+                    }
 
-                StringBuffer newUrl = new StringBuffer(stringWithParamName);
-                newUrl.append(payload.nextPayload(valueToModify));
-                newUrl.append(urlString.substring(lengthUntilParamStringEndFromStart));
+                    newUrl.append("&");
+                }
                 HtmlPage newPage = pageFetcher.getHtmlPageForUrl(newUrl.toString());
                 CompareSites compareSites = new CompareSites();
                 boolean isSameSite = compareSites.compare(originalPage.asText(), newPage.asText());
