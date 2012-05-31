d7c4b00c4069bddf31fd5c893ff4e4efc78e8739
Nataliya.Shevchenko <Nataliya.Shevchenko@gmail.com>
2012-05-31 16:22:50 +0000

    tests for at least one bracket in sql injection queries

--- src/test/java/de/nshevchenko/sqlinjection/type/DoubleQuotedStringSQLInjectionTest.java
+++ src/test/java/de/nshevchenko/sqlinjection/type/DoubleQuotedStringSQLInjectionTest.java
@@ -6,10 +6,18 @@ import org.junit.Test;
 
 public class DoubleQuotedStringSQLInjectionTest {
     @Test
-    public void testShouldAddExpectedString(){
+    public void testShouldAddExpectedStringWithZeroBrackets(){
         DoubleQuotedStringSQLInjection singleQuotedString = new DoubleQuotedStringSQLInjection();
         String generatedValue = singleQuotedString.createInjectionWithNumberOfBrackets("myValue", 0);
         Assert.assertTrue(generatedValue.matches("myValue\"\\sAND\\s\"\\w{4}\"=\"\\w{4}"));
+
+    }
+
+    @Test
+    public void testShouldAddExpectedStringForOneBracket(){
+        DoubleQuotedStringSQLInjection singleQuotedString = new DoubleQuotedStringSQLInjection();
+        String generatedValue = singleQuotedString.createInjectionWithNumberOfBrackets("myValue", 1);
+        Assert.assertTrue(generatedValue.matches("myValue\"\\)\\sAND\\s\\(\"\\w{4}\"=\"\\w{4}"));
 
     }
 }
