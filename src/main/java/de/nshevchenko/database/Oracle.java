5b88464cf6fc32228788ab83a18f6f202737c287
Nataliya.Shevchenko <Nataliya.Shevchenko@gmail.com>
2012-06-04 14:46:59 +0000

    oracle

--- src/main/java/de/nshevchenko/database/Oracle.java
+++ src/main/java/de/nshevchenko/database/Oracle.java
@@ -3,7 +3,11 @@ package de.nshevchenko.database;
 
 public class Oracle implements Database{
 
-    //select count (id) from reviews where review_author='MadBob' and select count(id) from reviws where review_author='MadB'||'ob' in oracle would produce the same result!
+    //select count (id) from reviews where review_author='MadBob'
+    // and
+    // select count(id) from reviews where review_author='MadB'||'ob'
+    // in oracle would produce the same result!
+    //pipe is used for concatenation in oracle!
 
     //check from oracle 10, not before!
     public boolean checkIfThisDatabase(){
