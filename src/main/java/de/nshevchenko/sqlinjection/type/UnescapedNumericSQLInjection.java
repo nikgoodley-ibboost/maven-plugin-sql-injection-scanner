02d5909c1f512931d3f40a70f9431ed69a7314c1
Nataliya.Shevchenko <Nataliya.Shevchenko@gmail.com>
2012-06-04 14:50:18 +0000

    infos on numeric slq injection

--- src/main/java/de/nshevchenko/sqlinjection/type/UnescapedNumericSQLInjection.java
+++ src/main/java/de/nshevchenko/sqlinjection/type/UnescapedNumericSQLInjection.java
@@ -5,6 +5,14 @@ public class UnescapedNumericSQLInjection implements SQLInjection{
 
     //action=artikel AND 229=229&cat=8&id=17&artlang=de
     //action=artikel&cat=8&id=17&artlang=de AND 95=95
+
+    //select review_content, review_author from reviews where id=5
+    //and
+    //select review_content, review_author from reviews where id=3+2 (10-5 or whatever)
+    //and
+    //select review_content, review_author from reviews where id=5+(Select 0/1)
+    //produce the same result!
+
     public String createInjectionWithNumberOfBrackets(String oldValue, int numberOfBrackets){
         int randomInt = (int)(Math.random()*10000);
         StringBuffer myNewValue = new StringBuffer(oldValue);
