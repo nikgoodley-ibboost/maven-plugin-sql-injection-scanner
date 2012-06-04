c4aa5c2c5ae54b9e757bb80b823b7f03fe5e8a6a
Nataliya.Shevchenko@gmail.com <Nataliya.Shevchenko@gmail.com@772861dd-1d62-cf6b-fec9-7aff7cb9eea6>
2012-06-04 14:46:17 +0000

    conactenating of string in oracle

--- trunk/src/main/java/de/nshevchenko/database/Oracle.java
+++ trunk/src/main/java/de/nshevchenko/database/Oracle.java
@@ -3,6 +3,8 @@ package de.nshevchenko.database;
 
 public class Oracle implements Database{
 
+    //select count (id) from reviews where review_author='MadBob' and select count(id) from reviws where review_author='MadB'||'ob' in oracle would produce the same result!
+
     //check from oracle 10, not before!
     public boolean checkIfThisDatabase(){
         return false;
