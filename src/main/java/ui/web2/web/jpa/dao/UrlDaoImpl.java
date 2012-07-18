44d3003ca0847f260667d66b9ff04b240918fa29
Nataliya.Shevchenko <Nataliya.Shevchenko@gmail.com>
2012-07-18 17:58:04 +0000

    creating folders and files

---
+++ src/main/java/ui/web2/web/jpa/dao/UrlDaoImpl.java
@@ -0,0 +1,47 @@
+package ui.web2.web.jpa.dao;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ui.web.org.springbyexample.web.jpa.bean.Person;
+import ui.web.org.springbyexample.web.jpa.bean.UrlToCheck;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Collection;
+
+@Repository
+@Transactional(readOnly = true)
+public class UrlDaoImpl implements UrlDao{
+
+    private EntityManager em = null;
+
+    /**
+     * Sets the entity manager.
+     */
+    @PersistenceContext
+    public void setEntityManager(EntityManager em) {
+        this.em = em;
+    }
+
+    @SuppressWarnings("unchecked")
+    public UrlToCheck findUrlToCheckById(Integer id){
+        return em.find(UrlToCheck.class, id);
+    }
+
+    @SuppressWarnings("unchecked")
+    public Collection<UrlToCheck> findUrlToCheck(){
+        return em.createQuery("select urls from UrlToCheck urls").getResultList();
+    }
+
+    @SuppressWarnings("unchecked")
+    public UrlToCheck save(UrlToCheck urlToCheck){
+        return em.merge(urlToCheck);
+    }
+
+    @SuppressWarnings("unchecked")
+    public void delete(UrlToCheck urlToCheck){
+        em.remove(em.merge(urlToCheck));
+    }
+
+
+}
