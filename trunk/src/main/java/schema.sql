9ff7e4aac402146b90c0cc326f7c7c400d613105
Nataliya.Shevchenko@gmail.com <Nataliya.Shevchenko@gmail.com@772861dd-1d62-cf6b-fec9-7aff7cb9eea6>
2012-07-23 14:01:59 +0000

    url schema in sql script

--- trunk/src/main/java/schema.sql
+++ trunk/src/main/java/schema.sql
@@ -4,6 +4,15 @@ CREATE TABLE IF NOT EXISTS url_report (
          started DATETIME,
          created DATETIME
        );
+
+CREATE TABLE IF NOT EXISTS url (
+         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+         url_string VARCHAR(100),
+         email_string VARCHAR(100),
+         created_date DATETIME,
+         log_file_name VARCHAR(100)
+       );
+
 create table IF NOT EXISTS address_report(
        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
        ZIP_POSTAL VARCHAR(10)
